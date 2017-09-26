3f8f7abe108f2227b4e754f5bc8613d2ad04b904
Tyson382 <tyson13032303@gmail.com>
2017-09-26 22:42:55 +0800

    [add]左側新增目錄選單頁面 新增目錄選單 即顯示當前目錄下之資料夾即檔案 並可進入資料夾且顯示資料夾底下目錄 //尚未做返回鍵 及開啟檔案之事件 及過濾非影片檔(.mp4, .avi)

--- Assets/GoogleVRProject/Script/LocalPathController.cs
+++ Assets/GoogleVRProject/Script/LocalPathController.cs
@@ -9,9 +9,11 @@ public class LocalPathController : MonoBehaviour
     private string PRE_FIX = "file://";
     private string _currentPath = "";
     private string[] _pathFiles;
+    private string[] _pathFolders;
     private int _pathLength;
 
-
+    public GameObject _fileButtonSample;
+    public GameObject _folderButtonSample;
     public GameObject _obbVideoSample;
 
 
@@ -21,20 +23,40 @@ public class LocalPathController : MonoBehaviour
         _currentPath = Directory.GetCurrentDirectory();
         _pathLength = Directory.GetFiles(_currentPath).Length;
         _pathFiles = new string[_pathLength];
-        
-        _pathFiles = Directory.GetFiles(_currentPath);
-        foreach(string pathFile in _pathFiles)
-            Debug.Log("pathFiles = " + pathFile);
+
+        this.GenerateCurrentPathObject();
+
         //_obbVideoSample.GetComponent<GvrVideoPlayerTexture>().videoURL = "file://"  + "/storage/MicroSD/111/TEST.mp4";
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.Find("Text").GetComponent<Text>().text = "Dir = " + _currentPath + ".";
+        transform.Find("CurrentLocalPathText").GetComponent<Text>().text = "Dir = " + _currentPath + ".";
         //transform.Find("Text").GetComponent<Text>().text = "Dir = " + _obbVideoSample.GetComponent<GvrVideoPlayerTexture>().videoURL + ".";
     }
 
+    public void OnFolderButtonClick(GameObject buttonObject)
+    {
+        this.DestoryPathObjectList();
+        _currentPath += "/" + buttonObject.transform.Find("Text").GetComponent<Text>().text;
+        Debug.Log("enter " + _currentPath + " folder!!");
+        this.GenerateCurrentPathObject();
+    } 
+
+    public void OnFileButtonClick(GameObject buttonObject)
+    {
+        this.DestoryPathObjectList();
+        _currentPath += "/" + buttonObject.transform.Find("Text").GetComponent<Text>().text;
+        Debug.Log("open " + _currentPath + " file!!");
+    }
+
+    private void DestoryPathObjectList()
+    {
+        Transform pathObjectList = this.transform.Find("FilesFoldersList");
+        foreach (Transform child in pathObjectList)
+            Destroy(child.gameObject);
+    }
     public static string GetAndroidInternalFilesDir()
     {
         string[] potentialDirectories = new string[]
@@ -56,5 +78,36 @@ public class LocalPathController : MonoBehaviour
             }
         }
         return "";
+    }
+
+    private void GenerateCurrentPathObject()
+    {
+        _pathFolders = Directory.GetDirectories(_currentPath);
+        foreach (string pathFolder in _pathFolders)
+        {
+            //Debug.Log("pathFolder = " + pathFolder);
+            this.GenerateFolderButtonUnderFolder(Path.GetFileName(pathFolder));
+        }
+
+        _pathFiles = Directory.GetFiles(_currentPath);
+        foreach (string pathFile in _pathFiles)
+        {
+            //Debug.Log("pathFiles = " + pathFile);
+            this.GenerateFileButtonUnderFolder(Path.GetFileName(pathFile));
+        }
+    }
+
+    private void GenerateFileButtonUnderFolder(string fileName)
+    {
+        GameObject cloneFileButton = Instantiate(_fileButtonSample, this.transform.Find("FilesFoldersList"));
+        cloneFileButton.transform.Find("Text").GetComponent<Text>().text = fileName;
+        cloneFileButton.SetActive(true);
+    }
+
+    private void GenerateFolderButtonUnderFolder(string folderName)
+    {
+        GameObject cloneFileButton = Instantiate(_folderButtonSample, this.transform.Find("FilesFoldersList"));
+        cloneFileButton.transform.Find("Text").GetComponent<Text>().text = folderName;
+        cloneFileButton.SetActive(true);
     }
 }
